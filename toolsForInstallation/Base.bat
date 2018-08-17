ee40b56c81e407959bcd2b3aa61466caddc6e6f3
goodidea82 <goodidea82 in gtaforums.com>
2018-08-17 19:59:53 +0200

    Fixed installation error: SA GTA3IM Changes\fences.txd not found

--- toolsForInstallation/Base.bat
+++ toolsForInstallation/Base.bat
@@ -18,17 +18,17 @@ REM xcopy requires that the full path is provided to xcopy_exclude and that no s
 for %%A in ("%CD%\toolsForInstallation\xcopy_exclude.txt") do set exclude=%%~sA
 set exclude=/EXCLUDE:%exclude%
 
-REM The buildIMGarchives creates .img files in the folder SAxVCxLC_source\base\models from the files in the folders ending with _img_src. 
-REM GitHub does not allow to upload the full img files. Also the idea of GitHub is to work with smaller files, so that individual files can be easily updated and tracked without causing much data traffic and storage.
-call buildIMGarchives.bat
-
 REM Some xcopy options.
 REM S= Use this option to copy directories, subdirectories, and the files contained within them, in addition to the files in the root of source. Empty folders will not be recreated.
 REM I= Use this option to show a list of the files and folders to copied... but no copying is actually done. The /l option is useful if you're building a complicated xcopy command with several options and you'd like to see how it would function hypothetically.
 REM Y= Use this option to stop the xcopy command from prompting you about overwriting files from source that already exist in destination.
 REM D=  Use the xcopy command with /d option and a specific date, in MM-DD-YYYY format, to copy files changed on or after that date. You can also use this option without specifying a specific date to copy only those files in source that are newer than the same files that already exist in destination. This is helpful when using the xcopy command to perform regular file backups.
-
 xcopy /SI %XCPYOptions% %exclude% "%fromDir%\base_root\*" "%toDir%\*"
+
+REM The buildIMGarchives creates .img files in the folder SAxVCxLC_source\base\models from the files in the folders ending with _img_src. 
+REM GitHub does not allow to upload the full img files. Also the idea of GitHub is to work with smaller files, so that individual files can be easily updated and tracked without causing much data traffic and storage.
+REM Attention buildIMGarchives.bat requires that some files were already copied from base_root
+call buildIMGarchives.bat
 
 if "%useModLoader%"=="1" (
 	REM Prepare directories
